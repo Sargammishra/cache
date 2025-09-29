cf3302f635e60b96bea655e1a1d235fa487cdf79
Sargammishra <sargammishra805@gmail.com>
2025-09-29 18:46:10 +0530

    jar updated

--- src/main/java/com/adcb/cache/config/RedisConfig.java
+++ src/main/java/com/adcb/cache/config/RedisConfig.java
@@ -3,26 +3,28 @@ package com.adcb.cache.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.url:redis://localhost:6379}")
-    private String redisUrl;
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private String redisPort;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        System.out.println("redisUrl "+redisUrl);
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisUrl));
+        return new LettuceConnectionFactory();
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-        return template;
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
     }
 }
